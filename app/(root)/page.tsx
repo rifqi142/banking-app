c7cc8aa8f21c358d7cb2ec2f98f89293e3ec0c36
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2024-05-15 23:59:06 +0700

    add: homepages chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 function Home() {
+  const loggedIn = { firstName: "Rifqi" };
+
   return (
-    <div>Home</div>
-  )
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently with our online banking services."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
